edf5c517bf4b47dcd74a0ffe392b90b8f982a328
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-22 15:18:47 -0700

    Rename remainder to mod

--- op_funcs3.c
+++ op_funcs3.c
@@ -94,3 +94,40 @@ void mul(stack_t **head, unsigned int line)
 		free(tmp);
 	}
 }
+
+/**
+ * mod - Calculate modulus of second element by the top element
+ * @head: Pointer to stack head
+ * @line: Monty bytecode line
+ * Return: Nothing
+ */
+void mod(stack_t **head, unsigned int line)
+{
+	stack_t *tmp = *head;
+	int mod;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line);
+		fclose(state.bfr);
+		free(state.info);
+		free_list(*head);
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line);
+		fclose(state.bfr);
+		free(state.info);
+		free_list(*head);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		mod = (*head)->next->n % (*head)->n;
+		(*head)->next->n = mod;
+		(*head) = (*head)->next;
+		(*head)->prev = NULL;
+		free(tmp);
+	}
+}
